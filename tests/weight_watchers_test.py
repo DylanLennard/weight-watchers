3ff6411cecbfecf6c71f48453efe2db456dfb7ac
DylanLennard <dlennard09@yahoo.com>
2020-08-09 09:21:06 -0700

    updated unit test suite

--- tests/weight_watchers_test.py
+++ tests/weight_watchers_test.py
@@ -1,4 +1,28 @@
-import weight_watchers
+from src import weight_watchers
+import vcr
+
+# some vars for use w/ unit tests below
+failure_html = "<html><p>nah</p></html>"
+success_html = f'''
+    <html>
+        <p class="ship-mode-message">
+        {weight_watchers.IN_STOCK_MSG}
+        </p>
+    </html>
+    '''
+req_list = [
+    {
+        "size": "25",
+        "url": "https://www.dickssportinggoods.com/p/ethos-25-" +
+               "lbolympic-rubber-bumper" +
+               "-plate-17au6uthsbmprpltswplb/17au6uthsbmprpltswplb"},
+    {
+        "size": "35",
+        "url": "https://www.dickssportinggoods.com/p/ethos-35-" +
+               "lbolympic-rubber-bumper-plate-" +
+               "17au6uthsbmprpltswplc/17au6uthsbmprpltswplc"
+    }
+]
 
 
 def test_get_endpoints_from_file():
@@ -7,21 +31,24 @@ def test_get_endpoints_from_file():
     assert len(req_list) != 0
 
 
-def test_request_endpoints_test():
-    pass
+@vcr.use_cassette("tests/fixtures/test_request_endpoints.yaml")
+def test_request_endpoints():
+    res = weight_watchers.request_endpoints(req_list)
+    for result in res:
+        assert "response" in result
 
 
-def check_for_success_test():
-    pass
+def test_check_for_success_test():
+    res_list = [{'response': success_html}, {'response': failure_html}]
+    res_list = weight_watchers.check_for_success(res_list)
+    for res in res_list:
+        assert 'available' in res
 
 
-def publish():
-    pass
+def test_is_available():
+    assert weight_watchers.is_available(success_html)
+    assert not weight_watchers.is_available(failure_html)
 
 
-def _is_available_test():
-    pass
-
-
-def run():
+def test_publish():
     pass
